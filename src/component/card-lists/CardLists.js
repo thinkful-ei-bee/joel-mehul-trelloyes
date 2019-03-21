42b8edf55f8625c5bda9c7d69151b4082f47af06
joelcorey <joelcorey@fastmail.com>
2019-03-21 13:24:42 -0700

    remove unneeded callback props

--- src/component/card-lists/CardLists.js
+++ src/component/card-lists/CardLists.js
@@ -3,15 +3,14 @@ import List from '../list/List'
 
 export default function CardLists(props) {
   
-  return (     <>
+  return (     
+    <>
       {props.lists.map((list, index) => 
       
         <List 
           key={index}
           id = {list.id}
           header={list.header}
-          // cardIds={list.cardIds}
-          // allCards={props.allCards}
           cards={list.cardIds.map(id => props.allCards[id])}
           onDeleteCard={props.onDeleteCard}
           onRandomCard={props.onRandomCard}
